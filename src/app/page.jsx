b6b72806902ca177a7202735a05b4a557bc46d55
Your Name <tauhid.swe@gmail.com>
2024-03-21 11:32:26 +0600

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,12 @@
 "use client"
 import CreatePost from "@/components/home/CreatePost";
 import ModalLayout from "@/components/reusable/ModalLayout";
-import Navbar from "@/components/shared/Navbar";
-import Sidebar from "@/components/shared/Sidebar";
 import Image from "next/image";
 import { useState } from "react";
+import { BiWorld } from "react-icons/bi";
 
 export default function Home() {
-  const [showModal, setShowModal] = useState(true);
+  const [showModal, setShowModal] = useState(false);
   // let showModal = true;
   // const setShowModal = () =>{
   //   showModal = !showModal;
@@ -16,7 +15,7 @@ export default function Home() {
   return (
     <main className="p-4">
       <div className="mx-10 space-x-6">
-        <CreatePost />
+        <CreatePost setShowModal ={setShowModal} />
       </div>
       <div>
         {
@@ -27,9 +26,11 @@ export default function Home() {
                 <Image className='h-12' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvts5aHBstDkR8PigS4RmZkbZy78zpZoSuOw&usqp=CAU" height={0} width={60} alt="" />
                 <div>
                   <h1>User Name</h1>
-                  <p className="text-[12px]">public</p>
+                  <p className="text-[12px] flex gap-1 items-center"><BiWorld/>public</p>
                 </div>
               </div>
+              <textarea className="w-full focus:outline-none resize-none m-2" name="post_description" id="post_description" rows="5" placeholder="What is in your mind?"></textarea>
+              <input className="bg-[#f0b1b1] w-full py-1 text-white rounded-lg" type="submit" value="Post" />
             </form>
           </ModalLayout>)
         }
--- src/components/reusable/ModalLayout.jsx
+++ src/components/reusable/ModalLayout.jsx
@@ -1,21 +1,22 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { CgClose } from "react-icons/cg";
 
-function ModalLayout({ children, setShowModal = (e: boolean) => { }, modalTitle = "" }) {
+
+function ModalLayout({ children, setShowModal, modalTitle = "" }) {
     // console.log(setShowModal);
     return (
         <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none border-none focus:outline-none bg-black/40 backdrop-blur-sm">
             <div className="relative w-auto my-6 ">
-                <div className="border-0  shadow-lg relative flex flex-col lg:w-[60vw] w-[90vw] max-h-[500px] overflow-y-auto bg-white border-none outline-none focus:outline-none">
-                    <div className="bg-gray-200 dark:bg-darkPrimary  p-3 border-none">
+                <div className="border-0  shadow-lg relative flex flex-col lg:w-[40vw] w-[95vw] max-h-[500px] overflow-y-auto bg-white border-none outline-none focus:outline-none">
+                    <div className="bg-white dark:bg-darkPrimary p-3 border-b border-gray-200">
                         <div className="relative">
-                            <h1 className="text-center text-gray-700 dark:text-gray-400 text-[24px] font-[600] uppercase border-none">
+                            <h1 className="text-center text-gray-700 dark:text-gray-400 font-semibold border-none">
                                 {" "}
                                 {modalTitle}
                             </h1>
                             <button
                                 type="button"
-                                className="absolute top-1 right-2  bg-white rounded-full text-black  dark:text-darkTextPrimary p-2 border-none"
+                                className="absolute -top-1 right-1  bg-gray-100 rounded-full text-black  dark:text-darkTextPrimary p-2 border-none"
                                 onClick={() => setShowModal(false)}
                             >
                                 <CgClose size={20} />
